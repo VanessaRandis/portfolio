d15acbd72a7db8488becfec1c8d2510db5d7828a
VanessaRandis <vanessa.randis@gmail.com>
2025-07-06 11:57:19 -0300

    includ js to project

---
+++ scripts/script-pt.js
@@ -0,0 +1,7 @@
+window.alert("Olá, Seja Bem vindo!")
+
+var nameAcess = window.prompt("Por favor informe nome")
+var celAcess = window.prompt("Por favor informe o celular")
+
+var callAfter = window.confirm("Posso entrar em contato para falar sobre as minhas expriências?")
+
